d655849b720797255f1b8d55f4e4443ceba956c0
hellenribas <tchwtrabalho@gmail.com>
2022-10-12 14:03:31 -0300

    feat: criacao rota get /proprietarios

--- app/app.py
+++ app/app.py
@@ -14,6 +14,17 @@ config = {
 
 connection = mysql.connector.connect(**config)
 
+@app.route('/proprietarios', methods=['GET'])
+def getProp():
+    cursor = connection.cursor()
+    query = 'SELECT * FROM proprietarios'
+    cursor.execute(query)
+    proprietarios = cursor.fetchall()
+    props = []
+    for prop in proprietarios:
+      obj = { "id": prop[0], "name": prop[1], "oportunidade_venda": prop[2] }
+      props.append(obj)
+    return jsonify({ "status": 200, "message": props })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)

fc2d3a1c1fe4bac78c57c9593faa99be581b5d6f
hellenribas <tchwtrabalho@gmail.com>
2022-10-12 20:17:48 -0300

    feat: criando crud com sqlalchemy

--- app/app.py
+++ app/app.py
@@ -1,30 +1,102 @@
-from flask import Flask, make_response, jsonify, request, render_template
+from flask import Flask, Response, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
+import json
 
 
 app = Flask(__name__)
 
-config = {
-        'user': 'carford',
-        'password': 'carford123',
-        'host': '172.22.0.2',
-        'port': '3306',
-        'database': 'carford_db'
-    }
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://carford:carford123@192.168.0.109/carford_db'
 
-connection = mysql.connector.connect(**config)
 
-@app.route('/proprietarios', methods=['GET'])
+connection = SQLAlchemy(app)
+
+
+# config = {
+#         'user': 'carford',
+#         'password': 'carford123',
+#         'host': '172.24.0.2',
+#         'port': '3306',
+#         'database': 'carford_db'
+#     }
+
+# connection = mysql.connector.connect(**config)
+
+class Proprietarios(connection.Model):
+  id = connection.Column("id", connection.Integer, primary_key=True)
+  nome = connection.Column("nome", connection.String(45))
+  oportunidade_venda = connection.Column("oportunidade_venda", connection.Boolean)
+
+  def __init__(self, nome, oportunidade_venda):
+    self.nome = nome
+    self.oportunidade_venda = oportunidade_venda
+  
+  def to_json(self):
+    return { "id": self.id, "nome": f"{self.nome}", "oportunidade_venda": self.oportunidade_venda }
+
+class Carros(connection.Model):
+  id = connection.Column('id', connection.Integer, primary_key=True)
+  modelo = connection.Column('modelo', connection.String(45))
+  cor = connection.Column('cor', connection.String(45))
+  proprietario_id = connection.Column('proprietario_id', connection.Integer)
+
+  def __init__(self, modelo, cor, proprietario_id):
+    self.modelo = modelo
+    self.cor = cor
+    self.proprietario_id = proprietario_id
+  
+  def to_json(self):
+    return { "id": self.id, "modelo": self.modelo, "cor": self.cor, "proprietario_id": self.proprietario_id }
+
+
+@app.route("/proprietarios", methods=['GET'])
 def getProp():
-    cursor = connection.cursor()
-    query = 'SELECT * FROM proprietarios'
-    cursor.execute(query)
-    proprietarios = cursor.fetchall()
-    props = []
-    for prop in proprietarios:
-      obj = { "id": prop[0], "name": prop[1], "oportunidade_venda": prop[2] }
-      props.append(obj)
-    return jsonify({ "status": 200, "message": props })
+  try:
+    proprietarios = Proprietarios.query.all()
+    json_convert = [ prop.to_json() for prop in proprietarios ]
+    return Response(json.dumps(json_convert))
+  except Exception as ex:
+    return Response(json.dumps({ "status": 404, "message": ex.args }))
+
+@app.route("/carros", methods=['GET'])
+def getCar():
+  try:
+    cars = Carros.query.all()
+    json_convert = [ car.to_json() for car in cars ]
+    return Response(json.dumps(json_convert))
+  except Exception as ex:
+    return Response(json.dumps({ "status": 404, "message": ex.args }))
+
+@app.route("/proprietario/<id>")
+def getPropId(id):
+  try:
+      proprietario = Proprietarios.query.filter_by(id=id).first()
+      json_convert = proprietario.to_json()
+      return Response(json.dumps(json_convert))
+  except Exception as ex:
+      return Response(json.dumps({ "status": 404, "message": ex.args }))
+
+@app.route("/carro/<id>")
+def getCarId(id):
+  try:
+      carro = Carros.query.filter_by(id=id).first()
+      json_convert = carro.to_json()
+      return Response(json.dumps(json_convert))
+  except Exception as ex:
+      return Response(json.dumps({ "status": 404, "message": ex.args }))  
+
+@app.route("/cadastro", methods=['POST'])
+def createProp():
+  body = request.get_json()
+
+  try:
+    proprietario = Proprietarios(nome=body["nome"], oportunidade_venda=body["oportunidade_venda"])
+    connection.session.add(proprietario)
+    connection.session.commit()
+    return Response(json.dumps(proprietario.to_json()))
+  except Exception as ex:
+    return Response(json.dumps({ "status": 404, "message": ex.args })) 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
